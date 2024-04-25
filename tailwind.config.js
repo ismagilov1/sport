11e9a36ff7d485418e9005250f6cdd00050aff39
Максим Стельмашенко <maximusdmrb@gmail.com>
2024-04-25 18:12:46 +0300

    add fonts, footer, logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        footer: "#7C3DE4",
+      },
+    },
   },
   plugins: [],
 };
