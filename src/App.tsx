f8199e8d7f19a8d4fea475fc9c3c6a869a2e50f1
ismagilov1 <ruulllo3@gmail.com>
2024-04-25 16:00:23 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Footer } from "./components/footer";
+import { Header } from "./components/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main></main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
